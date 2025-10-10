153c7062a82701c2b7911889a9f8214daad6f3ac
岩﨑健太郎 <iwasaki-ke@ibjapan.jp>
2025-10-10 10:46:12 +0900

    システム管理者のプロフィール画面でのヘッダーの修正及びパスワード変更処理の変更

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -11,22 +11,51 @@ class ProfilesController < ApplicationController
 
   def confirm
     @user = current_user
+
+    # パスワード変更なしがチェックされている場合は、パスワードパラメータを削除
+    user_params_copy = user_params.dup
+    if params[:skip_password_update].present?
+      user_params_copy.delete(:password)
+      user_params_copy.delete(:password_confirmation)
+      user_params_copy.delete(:current_password)
+    end
+
     # current_hospital_role_idを保持してから属性を更新
     original_current_hospital_role_id = @user.current_hospital_role_id
-    @user.assign_attributes(user_params.except(:current_password, :password, :password_confirmation))
+    @user.assign_attributes(user_params_copy.except(:current_password, :password, :password_confirmation))
     @user.current_hospital_role_id = original_current_hospital_role_id
-    
-    @has_password_change = user_params[:password].present?
-    
-    # パスワード変更がある場合は現在のパスワードをチェック
+
+    @has_password_change = user_params[:password].present? && user_params[:password_confirmation].present? && params[:skip_password_update].blank?
+    Rails.logger.debug "has_password_change判定結果: #{@has_password_change}"
+
+    # パスワードパラメータをビューで使用するために保存
+    @password = user_params[:password] if @has_password_change
+    @password_confirmation = user_params[:password_confirmation] if @has_password_change
+    @current_password = user_params[:current_password] if @has_password_change
+
+    # パスワード変更がある場合は現在のパスワードと新しいパスワードのバリデーションを行う
     if @has_password_change
       unless @user.valid_password?(user_params[:current_password])
         @user.errors.add(:current_password, "が正しくありません")
         render :edit, status: :unprocessable_entity
         return
       end
+
+      # 新しいパスワードのバリデーションを行う
+      temp_user = User.new(password: user_params[:password], password_confirmation: user_params[:password_confirmation])
+      unless temp_user.valid?
+        temp_user.errors.each do |attribute, message|
+          if attribute.to_s.start_with?('password')
+            @user.errors.add(attribute, message)
+          end
+        end
+        if @user.errors.any?
+          render :edit, status: :unprocessable_entity
+          return
+        end
+      end
     end
-    
+
     if @user.valid?
       render :confirm
     else
@@ -36,18 +65,28 @@ class ProfilesController < ApplicationController
 
   def update
     @user = current_user
-    
+
+    # パスワード変更なしがチェックされている場合は、パスワードパラメータを削除
+    if params[:skip_password_update].present?
+      params[:user].delete(:password)
+      params[:user].delete(:password_confirmation)
+    end
+
+    # パスワード変更がある場合はcurrent_passwordを保持、ない場合は除外
+    if user_params[:password].blank?
+      params[:user].delete(:current_password)
+    end
+
     # パスワード変更がある場合
     if user_params[:password].present?
-      # 現在のパスワードが正しいかチェック
-      unless @user.valid_password?(user_params[:current_password])
-        @user.errors.add(:current_password, "が正しくありません")
-        render :edit, status: :unprocessable_entity
-        return
-      end
-      
       # パスワード更新（Deviseのupdate_with_passwordを使用）
-      if @user.update_with_password(user_params)
+      password_params = user_params.slice(:current_password, :password, :password_confirmation)
+      if @user.update_with_password(password_params)
+        # パスワード更新成功後、名前とメールアドレスも更新
+        profile_params = user_params.except(:current_password, :password, :password_confirmation)
+        if profile_params.present?
+          @user.update(profile_params)
+        end
         bypass_sign_in(@user) # 再ログイン不要にする
         redirect_to profile_path, notice: 'プロフィールとパスワードを更新しました。'
       else
@@ -55,10 +94,8 @@ class ProfilesController < ApplicationController
       end
     else
       # パスワード変更なしの場合
-      # current_passwordを除外してupdate
-      update_params = user_params.except(:current_password, :password, :password_confirmation)
-      
-      if @user.update(update_params)
+      # ユーザー情報を更新
+      if @user.update(user_params)
         redirect_to profile_path, notice: 'プロフィールを更新しました。'
       else
         render :edit, status: :unprocessable_entity
